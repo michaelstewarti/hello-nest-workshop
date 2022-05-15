7bf80f3607041b9f127974615bf8da74a4a37629
Michael Stewart <michael.stewart@thecoursekey.com>
2022-05-15 18:42:11 -0400

    CP-3: add validation

--- src/expense-group/dto/create-expense-group.dto.ts
+++ src/expense-group/dto/create-expense-group.dto.ts
@@ -1,4 +1,9 @@
+import { IsString, Matches } from 'class-validator';
+
 export class CreateExpenseGroupDto {
+  @IsString()
   name: string;
+  @IsString()
+  @Matches(/^#(?:[0-9a-fA-F]{3}){1,2}$/)
   color: string;
 }
